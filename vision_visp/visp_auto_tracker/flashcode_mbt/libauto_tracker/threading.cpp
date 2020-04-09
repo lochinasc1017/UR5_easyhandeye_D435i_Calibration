9812a9cc6b71913bc61702baae35f3facf592dcb
lochinasc1017 <792258993@qq.com>
2020-04-09 10:06:57 +0800

    利用摄像头获得二维码位姿

---
+++ vision_visp/visp_auto_tracker/flashcode_mbt/libauto_tracker/threading.cpp
@@ -0,0 +1,8 @@
+#include "threading.h"
+
+TrackerThread::TrackerThread(tracking::Tracker& tracker) : tracker_(tracker){
+}
+
+void TrackerThread::operator()(){
+  tracker_.start(); //start state machine
+}
